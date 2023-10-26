9956fa11f0378819ccbfb6c7ccadfc4f2242087b
Pedro Cimini <ciminipedro@gmail.com>
2023-10-26 16:13:40 -0300

    Proto dropdowns

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,5 +1,44 @@
 ﻿<template>
   <ToastComponent ref="toast"></ToastComponent>
+
+  <div class="form-group col-10" style="display: flex; margin-top: 10px">
+    <label class="col-1">Depósito</label>
+    <div class="col-2">
+      <select v-model="DepositoSelecionado" class="form-control">
+        <option value=""></option>
+        <option
+          v-for="deposito in depositos"
+          :key="deposito.id"
+          :value="deposito.id"
+        >
+          {{ deposito.nome }}
+        </option>
+      </select>
+      <span name="depositoId" class="spanErro"></span>
+    </div>
+  </div>
+
+  <div class="form-group col-10" style="display: flex; margin-bottom;: 5px">
+    <label class="col-1">Material</label>
+    <div class="col-2">
+      <select
+        v-model="MaterialSelecionado"
+        class="form-control"
+        v-on:change="carregarUnidade()"
+      >
+        <option value=""></option>
+        <option
+          v-for="material in materiais"
+          :key="material.id"
+          :value="material.id"
+        >
+          {{ material.nome }}
+        </option>
+      </select>
+      <span name="materialId" class="spanErro"></span>
+    </div>
+  </div>
+
   <div style="width: 600px !important; height: 400px !important">
     <BarChart
       :labels="labelsMaterialPorData"
@@ -11,6 +50,7 @@
 
 <script>
 import ToastComponent from "../components/ToastComponent.vue";
+import axios from "axios";
 import BarChart from "@/components/BarChart.vue";
 import { mapState } from "vuex";
 
@@ -22,13 +62,51 @@ export default {
       dataMaterialPorData: [12, 5, 30],
       labelsMaterialPorData: ["teste1", "teste2", "teste3"],
       labelMaterialPorData: "teste",
+
+      entrada: {
+        id: this.$route.params.codigoEntrada,
+        depositoId: null,
+        materialId: null,
+        quantidade: null,
+        data: null,
+      },
+      unidadeMaterial: "?",
+      MaterialSelecionado: null,
+      DepositoSelecionado: null,
+      depositos: null,
+      materiais: null,
     };
   },
   computed: mapState(["sessaoValida"]),
   methods: {
-    carregar,
+    carregarDepositos() {
+      axios.post("deposito").then((result) => {
+        this.depositos = result.data;
+      });
+    },
+    carregarMateriais() {
+      axios.post("material").then((result) => {
+        this.materiais = result.data;
+      });
+    },
+    carregarUnidade() {
+      if (this.entrada.materialId > 0) {
+        axios
+          .post("material/carregarUnidadeMaterial", {
+            id: this.entrada.materialId,
+          })
+          .then((result) => {
+            this.unidadeMaterial = result.data.unidade;
+          });
+      } else {
+        this.unidadeMaterial = "?";
+      }
+    },
   },
-  mounted() {},
+  mounted() {
+    this.carregarDepositos();
+    this.carregarMateriais();
+  },
 };
 </script>
 
