26b255a5fce3b0545a831cc67165655dbb8a7920
PedC18 <PedCMF18@gmail.com>
2023-10-26 17:05:48 -0300

    Pie Chart Component

---
+++ client/src/components/PieChart.vue
@@ -0,0 +1,36 @@
+<template>
+  <canvas id="myPieChart"></canvas>
+</template>
+
+<script>
+import Chart from "chart.js/auto";
+
+export default {
+  name: "PieChart",
+  props: {
+    labels: null,
+    data: null,
+    label: null,
+  },
+  methods: {},
+  mounted() {
+    const ctx = document.getElementById("myPieChart");
+
+    new Chart(ctx, {
+      type: "pie",
+      data: {
+        labels: this.labels,
+        datasets: [
+          {
+            label: this.label,
+            data: this.data,
+            borderWidth: 1,
+          },
+        ],
+      },
+    });
+  },
+};
+</script>
+
+<style></style>
