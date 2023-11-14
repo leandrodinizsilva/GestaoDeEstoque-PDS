f14936d17ddeea722bbfe9f8af5a977f42b4a4ad
Hélio Martins <helio.1999.neto@gmail.com>
2023-11-14 20:27:26 -0300

    teste usuario existente

--- server/test/integration/usuario.cjs
+++ server/test/integration/usuario.cjs
@@ -13,5 +13,20 @@ describe('Usuario - Endpoints', () => {
             .send(USUARIO_VALIDO);
             response.should.have.status(200);
         });
+        it ('deve verificar usuário ja existente - 301', done => {
+            USUARIO_VALIDO = {'login':  'teste', 'nome': 'batata', 'senha': 1, 'tipo': 1}
+            chai.request('http://localhost:8000')
+            .post('/usuario/add')
+            .send(USUARIO_VALIDO)
+            .end((err, res) => {
+                chai.request('http://localhost:8000')
+                .post('/usuario/add')
+                .send(USUARIO_VALIDO)
+                .end((err, res) => {
+                    res.body.should.have.property('valido').equal(false);
+                    done();
+                });
+            });
+        });
     });
 });
