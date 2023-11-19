30616d9c03e76bcb564ab6a6fde593059968d1b8
Leandro Diniz <leandrodinizsilva9@gmail.com>
2023-11-19 20:45:55 -0300

    Novos testes e adicao de usuario

--- server/test/integration/usuario.cjs
+++ server/test/integration/usuario.cjs
@@ -6,25 +6,39 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 chai.should();
 
-async function logarNoSistema() {
+class Usuario{
+    constructor(nome, login, senha, tipo) {
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+        this.tipo = tipo;
+    }
+}
+
+async function logarNoSistema(usuario) {
     const responseLoginUsuario = await chai.request('http://localhost:8000')
     .post('/usuario/validar')
-    .send(USUARIO_VALIDO);
+    .send(usuario);
+}
 
-    return true;
+async function addUsuario(usuario) {
+    const response = await chai.request('http://localhost:8000')
+    .post('/usuario/add')
+    .send(usuario);
 }
 
 describe('Usuario - Endpoints', () => {
     describe('POST /api/usuario', () => {
         it ('deve retornar usuário criado - 201', async() => {
-            USUARIO_VALIDO = {'login':  'teste', 'nome': 'batata', 'senha': 1, 'tipo': 1}
+            USUARIO_VALIDO = new Usuario('teste', 'batata', 1, 1)
             const response = await chai.request('http://localhost:8000')
             .post('/usuario/add')
             .send(USUARIO_VALIDO);
             response.should.have.status(200);
+            response.body.should.have.property('valido').equal(true);
         });
         it ('deve verificar usuário ja existente - 301', done => {
-            USUARIO_VALIDO = {'login':  'teste', 'nome': 'batata', 'senha': 1, 'tipo': 1}
+            USUARIO_VALIDO = new Usuario('teste', 'batata', 1, 1)
             chai.request('http://localhost:8000')
             .post('/usuario/add')
             .send(USUARIO_VALIDO)
@@ -39,9 +53,8 @@ describe('Usuario - Endpoints', () => {
             });
         });
 
-
         it ('Deve retornar usuario logado', async() => {
-            USUARIO_VALIDO = {'login':  'UsuarioLogado', 'nome': 'UsuarioLogado', 'senha': 1, 'tipo': 3}
+            USUARIO_VALIDO = new Usuario ('UsuarioLogado', 'UsuarioLogado', 1, 3)
             const responseAddUser = await chai.request('http://localhost:8000')
             .post('/usuario/add')
             .send(USUARIO_VALIDO);
@@ -51,5 +64,35 @@ describe('Usuario - Endpoints', () => {
             responseLoginUsuario.should.have.status(200);
             responseLoginUsuario.body.should.have.property('valido').equal(true);
         });
+
+        it ('Deve retornar erro ao logar com usuario inexistente', async() => {
+            USUARIO_VALIDO = new Usuario('UsuarioRuim', 'UsuarioRuim', 'errada', 3)
+            const responseLoginUsuario = await chai.request('http://localhost:8000')
+            .post('/usuario/validar')
+            .send(USUARIO_VALIDO);
+            responseLoginUsuario.should.have.status(200);
+            responseLoginUsuario.body.should.have.property('valido').equal(false);
+        });
+
+        // it ('Deve retornar todos os 3 tipos de usuários', async() => {
+        //     const response = await chai.request('http://localhost:8000')
+        //     .post('/usuario/tipo')
+        //     .send('');
+        //     console.log(response.body)
+        //     response.should.have.status(200);
+        //     response.body.should.have.property('valido').equal(false);
+        // });
+
+        // it ('Deve retornar nome do usuário', async() => {
+        //     usuario = new Usuario('NomeLegal', 'LoginLegal', 1 , 3)
+        //     addUsuario(usuario)
+        //     logarNoSistema(usuario)
+        //     const response = await chai.request('http://localhost:8000')
+        //     .post('/usuario/carregarNome')
+        //     .send(usuario);
+        //     console.log(response.body)
+        //     response.should.have.status(200);
+        //     response.body.should.have.property('nome').equal('NomeLegal');
+        // });
     });
 });
